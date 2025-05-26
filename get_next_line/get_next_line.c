e21730f8d677df079460b36fdb55ac8223c75c01
djspright <gdlnolgs@gmail.com>
2025-05-26 16:37:05 +0900

    gnl

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: shkondo <shkondo@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/12 21:42:59 by shkondo           #+#    #+#             */
-/*   Updated: 2025/05/12 22:08:39 by shkondo          ###   ########.fr       */
+/*   Updated: 2025/05/26 16:31:37 by shkondo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,7 @@ char	*get_next_line(int fd)
 	if (fd < 0)
 		return (NULL);
 	i = 0;
+	line = NULL;
 	if (!saved_buffer)
 	{
 		while (i < BUFFER_SIZE)
@@ -59,27 +60,4 @@ char	*get_next_line(int fd)
 	if (EOF)
 		return (line);
 	return (NULL);
-}
-
-#include <fcntl.h>
-#include <stdio.h>
-
-int	main(void)
-{
-	int		fd;
-	char	*line;
-
-	fd = open("test.txt", O_RDONLY);
-	if (fd == -1)
-	{
-		perror("Error opening file");
-		return (1);
-	}
-	while ((line = get_next_line(fd)) != NULL)
-		printf("%s", line);
-	free(line);
-	line = NULL;
-	if (close(fd) == -1)
-		perror("Error closing file");
-	return (0);
 }
