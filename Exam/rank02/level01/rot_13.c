7106a1a6fd67b659bb1c4a8cc5ae6ea2cfac8269
Shotaro Kondo <shkondo@student.42tokyo.jp>
2025-08-02 21:08:14 +0900

    union

--- Exam/rank02/level01/rot_13.c
+++ Exam/rank02/level01/rot_13.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   union.c                                            :+:      :+:    :+:   */
+/*   rot_13.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: shkondo <shkondo@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/08/02 20:10:37 by shkondo           #+#    #+#             */
-/*   Updated: 2025/08/02 21:03:49 by shkondo          ###   ########.fr       */
+/*   Created: 2025/08/02 20:52:17 by shkondo           #+#    #+#             */
+/*   Updated: 2025/08/02 20:52:55 by shkondo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,31 +14,5 @@
 
 int	main(int ac, char **av)
 {
-	char	used[256] = {0};
-	int		i;
-
-	if (ac == 3)
-	{
-		i = 0;
-		while (av[1][i])
-		{
-			if (!used[(unsigned char)av[1][i]])
-			{
-				used[(unsigned char)av[1][i]] = 1;
-				write(1, &av[1][i], 1);
-			}
-			i++;
-		}
-		while (av[2][i])
-		{
-			if (!used[(unsigned char)av[2][i]])
-			{
-				used[(unsigned char)av[2][i]] = 1;
-				write(1, &av[2][i], 1);
-			}
-			i++;
-		}
-	}
-	write(1, "\n", 1);
 	return (0);
 }
--- Exam/rank02/level02/swap_bits.c
+++ Exam/rank02/level02/swap_bits.c
@@ -6,35 +6,35 @@
 /*   By: shkondo <shkondo@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/20 01:25:14 by shkondo           #+#    #+#             */
-/*   Updated: 2025/07/20 01:42:40 by shkondo          ###   ########.fr       */
+/*   Updated: 2025/08/02 20:07:36 by shkondo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void print_bits(unsigned char octet)
+void	print_bits(unsigned char octet)
 {
-  int div;
+	int	div;
 
-  div = 0b10000000;
-  while(div)
-  {
-    if(div & octet)
-      write(1, "1", 1);
-    else
-      write(1, "0", 1);
-    div >>= 1;
-  }
+	div = 0b10000000;
+	while (div)
+	{
+		if (div & octet)
+			write(1, "1", 1);
+		else
+			write(1, "0", 1);
+		div >>= 1;
+	}
 }
 
-unsigned char swap_bits(unsigned char octet)
+unsigned char	swap_bits(unsigned char octet)
 {
-  return ((octet >> 4) | (octet << 4));
+	return ((octet >> 4) | (octet << 4));
 }
 
-int main(void)
+int	main(void)
 {
-  print_bits(30);
-  write(1, "\n", 1);
-  print_bits(swap_bits(30));
+	print_bits(30);
+	write(1, "\n", 1);
+	print_bits(swap_bits(30));
 }
