df0051665e94c29122c1b3a10d60ab5c6e0c9448
djspright <gdlnolgs@gmail.com>
2025-08-19 14:32:06 +0900

    modified utils.c

--- fract-ol/src/utils.c
+++ fract-ol/src/utils.c
@@ -6,7 +6,7 @@
 /*   By: shkondo <shkondo@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/14 11:09:31 by shkondo           #+#    #+#             */
-/*   Updated: 2025/08/19 13:04:03 by shkondo          ###   ########.fr       */
+/*   Updated: 2025/08/19 14:31:02 by shkondo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,11 +48,27 @@ double	map_range(double value, double old_min, double old_max, double new_min,
 		+ new_min);
 }
 
-void color_interpolate(int start_color, int end_color, double t)
+int	color_interpolate(int start_color, int end_color, double t)
 {
-	int r = (int)((1 - t) * ((start_color >> 16) & 0xFF) + t * ((end_color >> 16) & 0xFF));
-	int g = (int)((1 - t) * ((start_color >> 8) & 0xFF) + t * ((end_color >> 8) & 0xFF));
-	int b = (int)((1 - t) * (start_color & 0xFF) + t * (end_color & 0xFF));
+	int	start_r;
+	int	start_g;
+	int	start_b;
+	int	r;
+	int	g;
+	int	b;
+	int	end_r;
+	int	end_g;
+	int	end_b;
+
+	start_r = (start_color >> 16) & 0xFF;
+	start_g = (start_color >> 8) & 0xFF;
+	start_b = start_color & 0xFF;
+	end_r = (end_color >> 16) & 0xFF;
+	end_g = (end_color >> 8) & 0xFF;
+	end_b = end_color & 0xFF;
+	r = (int)((1 - t) * start_r + t * end_r);
+	g = (int)((1 - t) * start_g + t * end_g);
+	b = (int)((1 - t) * start_b + t * end_b);
 	return (r << 16 | g << 8 | b);
 }
 
@@ -63,14 +79,17 @@ void	cleanup_exit(t_fractal *fractal)
 	if (fractal->win)
 		mlx_destroy_window(fractal->mlx, fractal->win);
 	if (fractal->mlx)
+	{
 		mlx_destroy_display(fractal->mlx);
+		free(fractal->mlx);
+	}
 	exit(0);
 }
 
 void	error_exit(const char *message, t_fractal *fractal)
 {
 	ft_putstr_fd("Error: ", 2);
-	ft_putstr_fd(message, 2);
+	ft_putstr_fd((char *)message, 2);
 	ft_putstr_fd("\n", 2);
 	cleanup_exit(fractal);
 }
